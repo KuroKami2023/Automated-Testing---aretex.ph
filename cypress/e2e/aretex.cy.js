020598a0a9079b4a0c97f60ee357041b2e101e89
Midorikami <150435939+KuroKami2023@users.noreply.github.com>
2024-05-08 17:06:17 +1000

    AT-aretex.ph Initial UPDATE: 05-08-2024

---
+++ cypress/e2e/aretex.cy.js
@@ -0,0 +1,84 @@
+const randomstring = require('randomstring');
+
+describe('Home', () => {
+  beforeEach( () => {
+    cy.visit('/')
+  })
+  it('Home should have contents', () => {
+    cy.contains('Home').click()
+    cy.url().should('include', '/home')
+    cy.get('nav').should('exist');
+    cy.scrollTo('bottom', { duration: 2000 })
+    cy.get('.bg-hero-image > :nth-child(1)').should('exist')
+    cy.get('.bg-hero-image > :nth-child(2)').should('exist')
+    cy.get('.bg-hero-image > :nth-child(2) > :nth-child(2)').should('exist')
+    cy.get('.bg-hero-image > :nth-child(2) > :nth-child(3)').should('exist')
+    cy.get(".md\\:flex-row > :nth-child(2)").should('exist');
+    cy.get('.bg-hero-image > :nth-child(4) > :nth-child(1)').should('exist')
+    cy.get('.flex-wrap > :nth-child(1) > .w-full').should('exist')
+    cy.get('.flex-wrap > :nth-child(2) > .w-full').should('exist')
+    cy.get('.flex-wrap > :nth-child(3) > .w-full').should('exist')
+    cy.scrollTo('top', { duration: 2000 })
+  })
+
+  it('should go to contact us and pass valid details', () => {
+
+    cy.get('.-mt-20 > .text-center > #forms > .py-4').click()
+    cy.url().should('include', '/contacts')
+    cy.get('.mt-12 > :nth-child(1) > .bg-white').type('Testing Message') //full name
+    cy.get('.mt-12 > :nth-child(2) > .bg-white').type('+631234567890') // phone number
+    cy.get('.mt-12 > :nth-child(2) >.bg-white').invoke('val').should('match', /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/)
+    cy.get('.mt-12 > :nth-child(3) >.bg-white').type('testing.email@aretex.com.au') // email
+    cy.get('.mt-12 > :nth-child(3) >.bg-white').invoke('val').should('match', /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+    cy.get('.mt-12 > :nth-child(4) > .bg-white').type(randomstring.generate(20)) //message
+
+    cy.get('.bg-orange-500').click();
+    cy.contains('Sending...').should('exist')
+  })
+
+  it('should go to another contact us and pass valid details', () => {
+
+    cy.get('.-mt-20 > .text-center > #forms > .py-4').click()
+
+    cy.get('.mt-12 > :nth-child(1) > .bg-white').type('Testing Message') //full name
+    cy.get('.mt-12 > :nth-child(2) > .bg-white').type('+631234567890') // phone number
+    cy.get('.mt-12 > :nth-child(3) >.bg-white').type('testing.email@aretex.com.au') // email
+    cy.get('.mt-12 > :nth-child(4) > .bg-white').type(randomstring.generate(20)) //message
+
+    cy.get('.bg-orange-500').click();
+    cy.contains('Sending...').should('exist')
+  })
+})
+
+describe('About', () => {
+  beforeEach( () => {
+    cy.visit('/')
+  })
+  it('About should have contents', () => {
+    cy.contains('About').click();
+    cy.url().should('include', '/about')
+    cy.scrollTo('bottom', { duration: 2000 })
+    cy.get('#root > :nth-child(2) > :nth-child(1) > :nth-child(1)').should('exist')
+    cy.get(':nth-child(2) > .py-10').should('exist')
+    cy.get('#root > :nth-child(2) > :nth-child(1) > .bg-primary > .bg-hero-image > .sm\\:px-16').should('exist')
+    cy.get('.-mt-40 > .sm\\:px-16 > :nth-child(2) > .sm\\:px-3').should('exist')
+    cy.get('.xl\\:max-w-\\[1280px\\] > .py-6').should('exist')
+    cy.scrollTo('top', { duration: 2000 })
+  }) 
+})
+
+Cypress.on('after:spec', (attributes) => {
+  console.log('Test "%s" has finished in %ds', 
+    attributes.title, (attributes.duration / 1000))
+})
+
+Cypress.on('after:spec', (results) => {
+  if (results) {
+    console.log(
+      results.totalPassed,
+      'out of',
+      results.totalTests,
+      'passed'
+    )
+  }
+})
